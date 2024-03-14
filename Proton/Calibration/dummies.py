5fa9725c5125efbce7456df7f8067d475e0bee1e
Bolun Han <Bolun.Han@outlook.com>
2024-03-14 16:52:16 +0800

    use session dummies from Quark

--- Proton/Calibration/dummies.py
+++ Proton/Calibration/dummies.py
@@ -1,3 +1,7 @@
-from ..Profile.cn import session_dummies, is_market_session, market_session_mask
+"""
+this script is designed to override market session related dummies and functions
+"""
+
+from Quark.Profile.cn import session_dummies, is_market_session, market_session_mask
 
 __all__ = ['session_dummies', 'is_market_session', 'market_session_mask']
